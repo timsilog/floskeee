fbcad4057952ade16fbfc182eb1fa29f93108298
Tim Jose <timjose3@gmail.com>
2019-06-13 01:49:16 -0700

    server

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,27 @@
 import React from 'react';
+import axios from 'axios';
 // import './App.css';
 
 function App() {
+  state = {
+    data: ''
+  }
+  componentDidMount = () => {
+    // Make sure to change the (localhost) on the line bellow 
+    // to the public DNS of your EC2 instance
+    // axios.get(`ec2-54-183-71-23.us-west-1.compute.amazonaws.com`)
+    axios.get(`http://localhost:4000/sayHello`)
+      .then(res => {
+        const dataFromServer = res.data;
+        this.setState({ data: dataFromServer });
+      });
+  }
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {this.state.data}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
